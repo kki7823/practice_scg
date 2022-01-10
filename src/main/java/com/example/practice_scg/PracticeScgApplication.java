93ece3a68dc21693f091df5db3f74c575c00c2c7
kki7823 <kjd55399997@gmail.com>
2022-01-10 18:22:33 +0900

    .

--- src/main/java/com/example/practice_scg/PracticeScgApplication.java
+++ src/main/java/com/example/practice_scg/PracticeScgApplication.java
@@ -2,7 +2,9 @@ package com.example.practice_scg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PracticeScgApplication {
     public static void main(String[] args) {
--- src/main/java/com/example/practice_scg/filter/GlobalFilter.java
+++ src/main/java/com/example/practice_scg/filter/GlobalFilter.java
@@ -4,10 +4,8 @@ import lombok.Data;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
